a382026b736a684e76d95e477b828b9ffac415bb
Samuel <belovedsamex@yahoo.com>
2023-04-14 01:18:23 -0700

    Deploy webstatic

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Compress web static package
-"""
+"""Compress web static package"""
 from fabric.api import *
 from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['100.25.200.66', '54.90.62.130']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-        """Deploy web files to server
-        """
+        """Deploy web files to server """
         try:
                 if not (path.exists(archive_path)):
                         return False
